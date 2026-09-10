c15faff8e3bb3008697a41942bda4162794e4a15
saaddar66 <saaddar66@hotmail.com>
2026-09-10 12:55:53 +0500

    Add computed field for internal transfer journal

--- models/account_move.py
+++ models/account_move.py
@@ -4,6 +4,13 @@ from odoo import models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    is_internal_transfer_journal = fields.Boolean(compute='_compute_is_internal_transfer_journal')
+
+    @api.depends('journal_id.name')
+    def _compute_is_internal_transfer_journal(self):
+        for move in self:
+            move.is_internal_transfer_journal = (move.journal_id.name == 'Internal Transfer')
+    
     def action_open_jv_cheque_wizard(self):
         self.ensure_one()
         return {
